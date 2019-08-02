2d17a5e895ced6440d349bd5a4e197459e276aea
themitrhil <53584947+themitrhil@users.noreply.github.com>
2019-08-01 22:05:54 -0300

    NavMeshAgent Simple Script
    Igual do albion

---
+++ CubeNavegation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CubeNavegation : MonoBehaviour
+{
+    [SerializeField]
+    private Camera Camera;
+    private NavMeshAgent navMeshAgent;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                Debug.Log("Did Hit");
+                navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
